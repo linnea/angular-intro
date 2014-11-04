ae7fe333e2983a5005b4c3142130b2c6143d655e
Linnea Watson <linnea_watson@hotmail.com>
2014-11-04 11:20:57 -0800

    Break

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@
 
 "use strict";
 
+angular.module('AngularDemo', [])
+    .controller('HelloController', function($scope){
+        $scope.person = {
+            firstName: null,
+            lastName: null
+        };
+
+
+    })
+
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies;
+    });
+
+     // chains methods so you don't have to create global variable
+    // controller declares and creates new controller
+    // scope object will be available to any views created in html file
